fd6404b09fcf6f77d529815db850f60cdb94df0c
svmatthews <matthews.sam@gmail.com>
2015-04-28 16:54:32 -0700

    make geojson instead of markers

--- js/site.js
+++ js/site.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-  console.log(map);
-});
+var map, gj;
+window.onload = init();
+
+function init() {
+  map = L.map('map').setView([0, 0], 1);
+  L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+    attribution: 'Stamen & OSM'
+  }).addTo(map);
+
+  gj = L.geoJson(geojson);
+  gj.addTo(map);
+
+  map.fitBounds(gj.getBounds());
+}
