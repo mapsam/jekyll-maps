104986a8dad5cf95d23e3173de16e2e33ea7c126
svmatthews <matthews.sam@gmail.com>
2015-04-28 18:09:07 -0700

    jekyll maps constructor to init app

--- js/site.js
+++ js/site.js
@@ -1,35 +1,38 @@
 ---
 ---
-var map, gj, 
-    posts = {
-      type: 'FeatureCollection',
-      features: [
-        {% for post in site.posts %}{
-          'type': 'Feature',
-          'properties': {},
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [
-              {{ post.lng }},
-              {{ post.lat }}
-            ]
-          }
-        },{% endfor %}
-      ]
-    };
 
-function init() {
-  map = L.map('map').setView([0, 0], 1);
-  L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-    attribution: 'Stamen & OSM'
-  }).addTo(map);
+var JekyllMaps = function ( options ) {
+  this.options = options || {};
+  this.map;
+  this.gj;
+  this.data = {
+    type: 'FeatureCollection',
+    features: [
+      {% for post in site.posts %}{
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [
+            {{ post.lng }},
+            {{ post.lat }}
+          ]
+        }
+      },{% endfor %}
+    ]
+  };
 
-  console.log(posts);
-
-  gj = L.geoJson(posts);
-  gj.addTo(map);
-
-  map.fitBounds(gj.getBounds());
+  this._createMap();
 }
 
-window.onload = init();
+JekyllMaps.prototype._createMap = function() {
+  this.map = L.map('map').setView([0, 0], 1);
+  L.tileLayer(this.options.tileset, {
+    attribution: '<a target="_blank" href="https://github.com/svmatthews/jekyll-maps">Jekyll Maps</a> | Tiles: Stamen & OSM'
+  }).addTo(this.map);
+
+  this.gj = L.geoJson(this.data);
+  this.gj.addTo(this.map);
+
+  this.map.fitBounds(this.gj.getBounds());
+}
